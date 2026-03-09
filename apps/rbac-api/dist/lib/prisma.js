5dbe261e75c194d1acbf5996b0ecaf2f5f33ad4f
llawrance <lijo.lawrence@gopuff.com>
2026-03-09 18:05:28 +0530

    removed unwanted code

--- apps/rbac-api/dist/lib/prisma.js
+++
@@ -1,4 +0,0 @@
-import { env } from "../config/env.js";
-import { PrismaClient } from "@prisma/client";
-process.env.DATABASE_URL = env.DATABASE_URL;
-export const prisma = new PrismaClient();
--- apps/web/.next_stale_1772695591/static/chunks/app/(protected)/reports/stock-movement/page-12390a25e3eadaeb.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2141],{},_=>{var e=e=>_(_.s=e);_.O(0,[4590,8753,9056,7069,7358],()=>e(78753)),_N_E=_.O()}]);
